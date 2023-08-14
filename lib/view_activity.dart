6788c6f00cc64b61ad60d70c7c6dead93d649581
Bilal <mbilal1980@hotmail.com>
2023-08-14 17:56:06 +0500

    Added option to sync and receive time from hardware

--- lib/view_activity.dart
+++ lib/view_activity.dart
@@ -27,11 +27,8 @@ class UuidConsts {
   static final OTA_SERVICE = Guid("1d14d6ee-fd63-4fa1-bfa4-8f47b42119f0");
   static final OTA_CONTROL = Guid("f7bf3564-fb6d-4e53-88a4-5e37e0326063");
   static final OTA_DATA = Guid("984227f3-34fc-4045-a5d0-2c581f81a153");
-
-  static final GENERIC_ACCESS = Guid("00001800-0000-1000-8000-00805f9b34fb");
-  static final DEVICE_NAME = Guid("00002a00-0000-1000-8000-00805f9b34fb");
-
-  static final CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR = Guid("00002902-0000-1000-8000-00805f9b34fb");
+  static final TIME = Guid("b8470d55-e009-41ff-a648-817e4488ba57");
+  static final BLINKY_SERVICE = Guid("de8a5aac-a99b-c315-0c80-60d4cbb51224");
 }
 
 enum States { idle, reconnecting, uploading, finalised }
@@ -56,6 +53,8 @@ class _ViewActivityState extends State<ViewActivity> {
   double bitrate = 0;
   BluetoothConnectionState connectionState = BluetoothConnectionState.disconnected;
   List<BluetoothService> list = [];
+  BluetoothCharacteristic? time;
+  DateTime? current_time;
 
   List<int> _getRandomBytes() {
     final math = Random();
@@ -69,7 +68,19 @@ class _ViewActivityState extends State<ViewActivity> {
     widget.device.servicesStream.listen((event) {
       setState(() {
         list = event;
+        time = event
+            .firstWhere((element) => element.serviceUuid == UuidConsts.BLINKY_SERVICE)
+            .characteristics
+            .firstWhere((element) => element.characteristicUuid == UuidConsts.TIME);
       });
+
+      if (time != null) {
+        time!.onValueReceived.listen((event) {
+          setState(() {
+            current_time = getDateFromBytes(event);
+          });
+        });
+      }
       var ctrlChar = event
           .firstWhere((element) => element.serviceUuid == UuidConsts.OTA_SERVICE)
           .characteristics
@@ -95,7 +106,9 @@ class _ViewActivityState extends State<ViewActivity> {
       });
 
       if (event == BluetoothConnectionState.connected) {
-        widget.device.requestMtu(255);
+        if (Platform.isAndroid) {
+          widget.device.requestMtu(255);
+        }
         Future.delayed(const Duration(milliseconds: 1000), () {
           widget.device.discoverServices();
         });
@@ -250,6 +263,50 @@ class _ViewActivityState extends State<ViewActivity> {
     }
   }
 
+  int getIntFromBytes(List<int> bytes) {
+    return (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
+  }
+
+  DateTime? getDateFromBytes(List<int> bytes) {
+    if (bytes.length != 36) {
+      return null;
+    }
+    int sec = getIntFromBytes(bytes.sublist(0, 4).reversed.toList());
+    int min = getIntFromBytes(bytes.sublist(4, 8).reversed.toList());
+    int hour = getIntFromBytes(bytes.sublist(8, 12).reversed.toList());
+    int day = getIntFromBytes(bytes.sublist(12, 16).reversed.toList());
+    int mon = getIntFromBytes(bytes.sublist(16, 20).reversed.toList()) + 1;
+    int year = getIntFromBytes(bytes.sublist(20, 24).reversed.toList()) + 1900;
+    int wday = getIntFromBytes(bytes.sublist(24, 28).reversed.toList());
+    int yday = getIntFromBytes(bytes.sublist(28, 32).reversed.toList());
+    int isdst = getIntFromBytes(bytes.sublist(32, 36).reversed.toList());
+
+    return DateTime(year, mon, day, hour, min, sec);
+  }
+
+  List<int> intToBytes(int value) {
+    List<int> bytes = [];
+    bytes.add((value >> 24) & 0xFF);
+    bytes.add((value >> 16) & 0xFF);
+    bytes.add((value >> 8) & 0xFF);
+    bytes.add(value & 0xFF);
+    return bytes.reversed.toList();
+  }
+
+  List<int> dateTimeToBytes() {
+    DateTime dateTime = DateTime.now();
+    List<int> bytes = [];
+    bytes.addAll(intToBytes(dateTime.second));
+    bytes.addAll(intToBytes(dateTime.minute));
+    bytes.addAll(intToBytes(dateTime.hour));
+    bytes.addAll(intToBytes(dateTime.day));
+    bytes.addAll(intToBytes(dateTime.month));
+    bytes.addAll(intToBytes(dateTime.year));
+    bytes.addAll(List<int>.generate(12, (index) => 0));
+
+    return bytes;
+  }
+
   @override
   Widget build(BuildContext context) {
     VoidCallback? onPressed;
@@ -315,6 +372,61 @@ class _ViewActivityState extends State<ViewActivity> {
                       }
                     }),
               ),
+              (time != null)
+                  ? ListTile(
+                      title: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const Text('Time on device'),
+                        Text(
+                          (current_time == null) ? '' : current_time!.toIso8601String(),
+                          style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).textTheme.bodySmall?.color),
+                        ),
+                        if (time!.properties.write)
+                          Row(
+                            children: <Widget>[
+                              if (time!.properties.read)
+                                TextButton(
+                                    child: const Text("Read"),
+                                    onPressed: () async {
+                                      try {
+                                        await time!.read();
+                                      } catch (e) {
+                                        final snackBar = SnackBar(content: Text(prettyException("Read Error:", e)));
+                                        snackBarKeyC.currentState?.showSnackBar(snackBar);
+                                      }
+                                    }),
+                              if (time!.properties.write)
+                                TextButton(
+                                    child: const Text("Sync"),
+                                    onPressed: () async {
+                                      try {
+                                        await time!.write(dateTimeToBytes());
+                                      } catch (e) {
+                                        final snackBar = SnackBar(content: Text(prettyException("Subscribe Error:", e)));
+                                        snackBarKeyC.currentState?.showSnackBar(snackBar);
+                                      }
+                                    }),
+                              if (time!.properties.notify || time!.properties.indicate)
+                                TextButton(
+                                    child: Text(time!.isNotifying ? "Unsubscribe" : "Subscribe"),
+                                    onPressed: () async {
+                                      try {
+                                        await time!.setNotifyValue(time!.isNotifying == false);
+                                        if (time!.properties.read) {
+                                          await time!.read();
+                                        }
+                                      } catch (e) {
+                                        final snackBar = SnackBar(content: Text(prettyException("Subscribe Error:", e)));
+                                        snackBarKeyC.currentState?.showSnackBar(snackBar);
+                                      }
+                                    })
+                            ],
+                          ),
+                      ],
+                    ))
+                  : const Text("Time not found"),
               Column(
                 children: [
                   Row(
